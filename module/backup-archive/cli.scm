f5a0b488d5906a84f82399f59f9dd23b7a928228
nfdisco <nr9@posteo.de>
2019-06-16 23:12:44 +0200

    First working version.

--- module/backup-archive/cli.scm
+++ module/backup-archive/cli.scm
@@ -1,15 +1,15 @@
 (define-module (backup-archive cli)
   #:use-module (backup-archive program)
   #:use-module (backup-archive message)
-  #:use-module (backup-archive profile)
+  #:use-module (backup-archive backup)
   #:use-module (ice-9 getopt-long)
   #:export     (main))
 
 (define (display-help)
   "Print help message."
   (format #t "\
-Usage: ~a [OPTIONS]
-Backup data with find and tar.
+Usage: ~a [OPTIONS] [BACKUP-ID]
+Backup data using find and tar.
 
 Options:
      -h, --help     show this help and exit
@@ -18,7 +18,7 @@ Options:
 
 (define (display-version)
   "Print version string."
-  (format #t "~a version ~a\n" (program-name) archive-backup-version))
+  (format #t "~a version ~a\n" (program-name) backup-archive-version))
 
 (define option-spec
   '((help    (single-char #\h) (value #f))
@@ -26,14 +26,21 @@ Options:
 
 (define (%main args)
   "Process command-line arguments."
-  (let ((options (getopt-long args option-spec)))
-    (cond ((option-ref options 'help #f)
-           (display-help))
-          ((option-ref options 'version #f)
-           (display-version))
-          (else
-           (format #t "~s\n"
-                   (read-profiles (car (option-ref options '() #f))))))))
+  (let* ((options      (getopt-long args option-spec))
+         (non-opt-args (option-ref options '() '())))
+    (when (option-ref options 'help #f)
+          (display-help)
+          (exit 0))
+    (when (option-ref options 'version #f)
+          (display-version)
+          (exit 0))
+    (when (null? non-opt-args)
+          (msg-error "non-option argument required")
+          (exit 1))
+    (when (not (null? (cdr non-opt-args)))
+          (msg-error "too many non-option arguments")
+          (exit 1))
+    (make-backup (car non-opt-args))))
 
 (define (main args)
   (parameterize ((program-name (basename (car args))))
