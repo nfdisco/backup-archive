b7a748686507531ccc37689421f15e5aa9862eea
nfdisco <nr9@posteo.de>
2019-08-15 16:44:50 +0200

    Use split(1) to write archive.

--- module/backup-archive/archive.scm
+++ module/backup-archive/archive.scm
@@ -2,12 +2,10 @@
   #:use-module (backup-archive common)
   #:use-module (backup-archive shell)
   #:use-module (ice-9 regex)
+  #:use-module (ice-9 format)
   #:export     (archive-format-supported?
-                archive-format-suffix
-                archive-format-filters
-                archive-find
-                archive-last
-                archive-current
+                archive-find-most-recent
+                archive-prefix-current
                 archive-write))
 
 (define archive-format-alist
@@ -37,52 +35,84 @@ archive format."
         #f
         (cadr value))))
 
-(define (archive-filename-pattern name)
-  "Return regular expression that matches NAME archive filenames."
-  (make-regexp (string-append "^" (regexp-quote name) "-([0-9]+)([.].+)?$")))
+(define (archive-filename-pattern prefix)
+  "Return filename pattern."
+  (string-append "^" (regexp-quote prefix) "-([0-9]+)-([a-z]+)([.].+)?$"))
 
-(define (archive-filename name n fmt)
-  "Return archive filename."
-  (string-append name "-" (number->string n) (archive-format-suffix fmt)))
+(define (archive-filename-volume match)
+  "Return volume component."
+  (and match
+       (string->number (match:substring match 1))))
 
-(define (archive-find name dir fmt)
-  "Return the pathnames of last and current archive."
+(define (archive-filename-part match)
+  "Return part component."
+  (and match
+       (match:substring match 2)))
+
+(define (archive-filename match)
+  "Return whole filename."
+  (and match
+       (match:string match)))
+
+(define (archive-filename-parser prefix)
+  "Return filename-parsing procedure."
+  (let ((pattern (make-regexp (archive-filename-pattern prefix))))
+    (lambda (filename)
+      (regexp-exec pattern filename))))
+
+(define (archive-filename-prefix name volume)
+  "Return archive filename prefix."
+  (format #f "~a-~3,'0d-" name volume))
+
+(define (archive-prefix-current name last)
+  "Return filename prefix for current archive."
+  (if last
+      (let ((match ((archive-filename-parser name) last)))
+        (archive-filename-prefix name
+                                 (1+ (archive-filename-volume match))))
+      (archive-filename-prefix name 0)))
+
+(define (archive-find-most-recent name dir)
+  "Return filename of most recent archive."
   (let ((stream (opendir dir))
-        (pattern (archive-filename-pattern name)))
-    (define (iter last)
+        (f (archive-filename-parser name)))
+    (define (iter match)
       (let ((filename (readdir stream)))
-        (if (eof-object? filename)
-            last
-            (let* ((match (regexp-exec pattern filename))
-                   (n (and (regexp-match? match)
-                           (string->number (match:substring match 1)))))
-              (if (and n
-                       (> n (cdr last)))
-                  (iter (cons filename n))
-                  (iter last))))))
-    (let ((last (iter '(#f . -1))))
-      (closedir stream)
-      (cons (if (car last)
-                (path-join dir (car last))
-                #f)
-            (path-join dir (archive-filename name (1+ (cdr last)) fmt))))))
+        (cond ((eof-object? filename)
+               (closedir stream)
+               (and match
+                    (archive-filename match)))
+              (else
+               (let ((match-current (f filename)))
+                 (cond ((not match-current)
+                        (iter match))
+                       ((not match)
+                        (iter match-current))
+                       (else
+                        (if (> (archive-filename-volume match-current)
+                               (archive-filename-volume match))
+                            (iter match-current)
+                            (iter match)))))))))
+    (iter #f)))
 
-(define archive-last car)
-
-(define archive-current cdr)
-
-(define (archive-write output-file backup-files filters)
+(define (archive-write output-prefix backup-files archive-format)
   "Write archive file."
-  (with-output-to-file output-file
-    (lambda ()
-      ((apply shell-pipe
-              (cons '("tar"
-                      "--no-recursion"
-                      "--null"
-                      "--files-from" "-"
-                      "--preserve-permissions"
-                      "--create"
-                      "--totals")
-                    filters))
-       (lambda () (display backup-files))))))
-
+  ((apply shell-pipe
+          `(("tar"
+             "--no-recursion"
+             "--null"
+             "--files-from"
+             "-"
+             "--preserve-permissions"
+             "--create"
+             "--totals")
+            ,@(archive-format-filters archive-format)
+            ("split"
+             "--additional-suffix"
+             ,(archive-format-suffix archive-format)
+             "-b"
+             "1G"
+             "-d"
+             "-"
+             ,output-prefix)))
+   (lambda () (display backup-files))))
